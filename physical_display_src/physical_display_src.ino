330f82e4f55d22fc243bafdc8facbe1b6f2f740d
Ryan Maloney <malr@spooky.dog>
2024-01-02 12:18:29 +0000

    created included library references and added configure_matrix_display function

--- physical_display_src/physical_display_src.ino
+++ physical_display_src/physical_display_src.ino
@@ -7,12 +7,18 @@ with a Waveshare 32x64 LED Matrix display.
 
 Styling Guidelines followed: https://www.cs.umd.edu/~nelson/classes/resources/cstyleguide/
 */
+#include "ESP32-HUB75-MatrixPanel-I2S-DMA.h"
+#include "Fonts/Picopixel.h"
+#include <ArduinoJson.h>
+#include <ArduinoJson.hpp>
+#include <WiFi.h>
+#include <HTTPClient.h>
 
 #define PANEL_RES_X 64
 #define PANEL_RES_Y 32
 #define PANEL_CHAIN 1
-#define YELLOW 0XFFE0
-#define TEXT_SIZE 1
+#define DISPLAY_YELLOW 0XFFE0
+#define DISPLAY_TEXT_SIZE 1
 #define WIFI_SSID ssid_here
 #define WIFI_PASSWORD ssid_password_here
 #define API_ENDPOINT_ADDRESS endpoint_address_here
@@ -40,5 +46,21 @@ void setup() {
 }
 
 void loop() {
-
 }
+
+void configure_matrix_display(){
+  /*
+  Configure the Matrix Display initially
+  with cursor position; style, size and colour
+  of font; as well as brightness
+  */
+  train_display_panel->begin();
+  train_display_panel->setBrightness(255);
+  train_display_panel->fillScreen(train_display_panel->color444(0, 0, 0));
+  train_display_panel->setCursor(0, 5);
+  train_display_panel->setTextColor(DISPLAY_YELLOW);
+  train_display_panel->setTextSize(DISPLAY_TEXT_SIZE);
+  train_display_panel->setFont(&Picopixel);
+}
+void connect_to_wifi(){}
+
